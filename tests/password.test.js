459546a1032f6b998eb593c2f215360fe2bcddd0
RohithRaoP <rohithraopogala3@gmail.com>
2026-03-24 23:14:46 +0530

    Add tests for password strength validation

--- tests/password.test.js
+++ tests/password.test.js
@@ -1,1 +1,17 @@
+const isStrongPassword = require('./password');
 
+test('valid strong password', () => {
+  expect(isStrongPassword('DevOps123')).toBe(true);
+});
+
+test('fails for short password', () => {
+  expect(isStrongPassword('Dev1')).toBe(false);
+});
+
+test('fails without uppercase', () => {
+  expect(isStrongPassword('devops123')).toBe(false);
+});
+
+test('fails without number', () => {
+  expect(isStrongPassword('DevOps')).toBe(false);
+});
