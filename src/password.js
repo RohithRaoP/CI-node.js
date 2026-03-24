b93e55b22aacb5d1e6d66dd5a859bb00906cafe0
RohithRaoP <rohithraopogala3@gmail.com>
2026-03-24 23:14:09 +0530

    Implement strong password validation function
    Add a function to validate strong passwords based on length, uppercase letters, and digits.

--- src/password.js
+++ src/password.js
@@ -1,1 +1,8 @@
+function isStrongPassword(password) {
+  if (password.length < 8) return false;
+  if (!/[A-Z]/.test(password)) return false;
+  if (!/[0-9]/.test(password)) return false;
+  return true;
+}
 
+module.exports = isStrongPassword;
